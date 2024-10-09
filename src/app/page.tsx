c8c02f6a261321ef68acd2ec51424832a6d1ffd7
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-10-09 20:12:28 -0300

    added color selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Accordion from "@/components/accordion";
+import RandomColor from "@/components/random-color";
 
 export default function Home() {
   return (
     <div className="app">
       {/* Accordion component  */}
-      <Accordion />
+      {/* <Accordion /> */}
+
+      {/* Random Color component */}
+      <RandomColor />
     </div>
   );
 }
