3e485a7848e0cbd47d2fee9c1dd3e3df008b226b
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-10-12 18:15:04 -0300

    added image slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Accordion from "@/components/accordion";
+import ImageSlider from "@/components/image-slider";
 import RandomColor from "@/components/random-color";
 import StarRating from "@/components/star-rating";
 
@@ -12,7 +13,10 @@ export default function Home() {
       {/* <RandomColor /> */}
 
       {/* Star rating components */}
-      <StarRating noOfStars={6} />
+      {/* <StarRating noOfStars={6} /> */}
+
+      {/* Image slider component */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} />
     </div>
   );
 }
