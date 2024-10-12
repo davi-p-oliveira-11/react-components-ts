d945c2ac1dfc8b4c4727593f03484e69cfcec889
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-10-12 19:38:52 -0300

    added load more component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Accordion from "@/components/accordion";
 import ImageSlider from "@/components/image-slider";
+import LoadMoreData from "@/components/load-more-data";
 import RandomColor from "@/components/random-color";
 import StarRating from "@/components/star-rating";
 
@@ -16,7 +17,10 @@ export default function Home() {
       {/* <StarRating noOfStars={6} /> */}
 
       {/* Image slider component */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} />
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} /> */}
+
+      {/* Load More component */}
+      <LoadMoreData />
     </div>
   );
 }
