2b220afa801a85cacf041a0f4243cc58d927a35c
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-10-12 16:16:09 -0300

    added star-rating component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Accordion from "@/components/accordion";
 import RandomColor from "@/components/random-color";
+import StarRating from "@/components/star-rating";
 
 export default function Home() {
   return (
@@ -8,7 +9,10 @@ export default function Home() {
       {/* <Accordion /> */}
 
       {/* Random Color component */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+
+      {/* Star rating components */}
+      <StarRating noOfStars={6} />
     </div>
   );
 }
